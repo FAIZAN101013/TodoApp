a748faa68b61746d716d0f3ba536ccd3ba3faa9a
FAIZAN101013 <faizan.m.patel10@gmail.com>
2026-09-01 16:38:00 +0530

    feat: wire Home screen into navigation stack

--- App.tsx
+++ App.tsx
@@ -1,19 +1,44 @@
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {
+  NavigationContainer,
+} from '@react-navigation/native';
+import {
+  createNativeStackNavigator,
+} from '@react-navigation/native-stack';
 
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
+import HomeScreen from './screens/HomeScreen';
 
-const Stack = createNativeStackNavigator();
+export type RootStackParamList = {
+  Login: undefined;
+  Register: undefined;
+  Home: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{title: 'Login'}}
+        />
 
-        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen
+          name="Register"
+          component={RegisterScreen}
+          options={{title: 'Register'}}
+        />
+
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'My Todo List'}}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
